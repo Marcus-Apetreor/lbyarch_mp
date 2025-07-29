9b8b7054f25334516da86eaf1f959a59072c9d64
Marcus-Apetreor <126288594+Marcus-Apetreor@users.noreply.github.com>
2025-07-29 23:56:19 +0800

    updated to match other .asm

--- asm LBYARCH_MP/imgconvert.asm
+++ asm LBYARCH_MP/imgconvert.asm
@@ -1,38 +1,33 @@
-; convert.asm - Windows x64 MASM-compatible ASM (Visual Studio)
 section .data
 scalar_255: dd 255.0
 
 section .text
 bits 64
 default rel
-global imgconvert
+global imgCvtGrayFloatToInt
 
-imgconvert:
-    ; Windows x64 calling convention
+imgCvtGrayFloatToInt:
     ; RCX = f_img (float*)
     ; RDX = i_img (uint8_t*)
-    ; R8  = size  (int)
+    ; R8  = size (int)
 
     push rbp
     push rsi
     push rdi
-
-    mov rsi, rcx      ; rsi = f_img
-    mov rdi, rdx      ; rdi = i_img
-    mov ecx, r8d      ; ecx = size (use ecx for loop)
-
-    xor rbx, rbx      ; rbx = loop index
+    mov rsi, rcx ; rsi = f_img (float img)
+    mov rdi, rdx ; rdi = i_img (int8 img)
+    mov ecx, r8d ; ecx = size (use ecx for loop)
+    xor rbx, rbx ; rbx = loop idx
 
 .loop:
     cmp rbx, rcx
     jge .done
 
-    ; load float from [rsi + rbx*4]
-    movss xmm0, dword [rsi + rbx*4]       ; load float
-    movss xmm1, dword [rel scalar_255]    ; xmm1 = 255.0
-    mulss xmm0, xmm1                      ; xmm0 *= 255.0
-    cvtss2si eax, xmm0                    ; eax = int(round(xmm0))
-    mov [rdi + rbx], al                   ; store in int buffer
+    movss xmm0, dword [rsi + rbx*4] ; load float
+    movss xmm1, dword [rel scalar_255] ; xmm1 = 255.0
+    mulss xmm0, xmm1 ; xmm0 *= 255.0
+    cvttss2si eax, xmm0 ; eax = int(round(xmm0))
+    mov [rdi + rbx], al ; store in int buffer
 
     inc rbx
     jmp .loop
